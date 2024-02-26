20c261b42100ada774e97b14fd6b4db8ce97c77c
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2024-02-26 01:44:41 -0400

    Add files via upload

---
+++ 1052 - Mês.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <iostream>
+ 
+int main() {
+    
+    int numero;
+    
+    std::cin >> numero;
+    
+    if(numero == 1){
+        
+        std::cout << "January" <<std::endl;
+        
+    }else if(numero == 2){
+        
+        std::cout << "February" <<std::endl;
+        
+    }else if(numero == 3){
+        
+        std::cout << "March" <<std::endl;
+        
+    }else if(numero == 4){
+        
+        std::cout << "April" <<std::endl;
+        
+    }else if(numero == 5){
+        
+        std::cout << "May" <<std::endl;
+        
+    }else if(numero == 6){
+        
+        std::cout << "June" <<std::endl;
+        
+    }else if(numero == 7){
+        
+        std::cout << "July" <<std::endl;
+        
+    }else if(numero == 8){
+        
+        std::cout << "August" <<std::endl;
+        
+    }else if(numero == 9){
+        
+        std::cout << "September" <<std::endl;
+        
+    }else if(numero == 10){
+        
+        std::cout << "October" <<std::endl;
+        
+    }else if(numero ==11){
+        
+        std::cout << "November" <<std::endl;
+        
+    }else{
+        
+        std::cout << "December" <<std::endl;
+        
+    }
+ 
+ 
+    return 0;
+}
